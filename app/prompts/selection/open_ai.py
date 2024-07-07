b6bd73a956f0adafdd9e57fecc4b28c8684de03d
jinyang628 <chenjinyang4192@gmail.com>
2024-07-06 19:21:03 -0700

    Update

--- app/prompts/selection/open_ai.py
+++ app/prompts/selection/open_ai.py
@@ -6,7 +6,7 @@ def generate_openai_selection_system_message() -> str:
     return f"""Your task is to interpret the user's natural language instruction and select the relevant (task, application, table name, HTTP method) groupings so that an ORM can use your output to perform specific actions on the databases of applications.
 
 Follow these guidelines:
-    1. The user's instruction might compose of multiple tasks and you need to split up his instruction into multiple sub-tasks. Rephrase or summarise if necessary.
+    1. The user's instruction might compose of multiple tasks and you need to split up his instruction into multiple sub-tasks. Rephrase or summarise if necessary, but do not leave out any necessary information when describing the sub-task.
     2. For each task, it might involve requests to multiple applications and you need to decide which subset of applications the task is related to.
     3. For each application, you need to determine the subset of tables which the task is related to.
     4. For each table, you need to determine the appropriate HTTP methods to use given the task.  
