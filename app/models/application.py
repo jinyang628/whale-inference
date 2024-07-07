4367128a9069e3df319d6d45cb1cb0374539bbeb
jinyang628 <chenjinyang4192@gmail.com>
2024-07-06 21:08:06 -0700

    update

--- app/models/application.py
+++ app/models/application.py
@@ -1,6 +1,6 @@
 from enum import StrEnum
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, model_validator
+from typing import Any, Optional
 
 class DataType(StrEnum):
     STRING = "string"
@@ -17,8 +17,41 @@ class PrimaryKey(StrEnum):
 class Column(BaseModel):
     name: str
     data_type: DataType
-    nullable: bool = False
     primary_key: PrimaryKey = PrimaryKey.NONE
+    nullable: bool = False
+    default_value: Optional[Any] = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def set_default_value(cls, data: Any) -> Any:
+        
+        if not isinstance(data, dict):
+            raise ValueError("Column data must be a dictionary.")
+        
+        # If the default value is set, use it
+        if ("default_value" in data):
+            return data
+        
+        if "nullable" in data:
+            if data["nullable"]:
+                # If the column is nullable, the default value is None
+                return data
+            
+        data_type = data["data_type"]
+        if data_type == DataType.STRING:
+            data["default_value"] = ""
+        elif data_type == DataType.INTEGER:
+            data["default_value"] = 0
+        elif data_type == DataType.FLOAT:
+            data["default_value"] = 0.0
+        elif data_type == DataType.BOOLEAN:
+            data["default_value"] = False
+        elif data_type == DataType.DATETIME:
+            data["default_value"] = (
+                "1970-01-01T00:00:00Z"  # ISO format for datetime
+            )
+            
+        return data
 
 class Table(BaseModel):
     name: str
