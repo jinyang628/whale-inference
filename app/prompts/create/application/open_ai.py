395c949757ca9abe444368b915023ba3d608b45a
jinyang628 <chenjinyang4192@gmail.com>
2024-07-28 14:42:12 -0700

    Update

--- app/prompts/create/application/open_ai.py
+++ app/prompts/create/application/open_ai.py
@@ -2,7 +2,7 @@ from app.models.inference.create import CreateMessage
 
 
 def generate_openai_application_system_message() -> str:
-    return f"""Your task is create an application based on the user's description. You need to generate the database tables necessary to support the CRUD operations, and provide a description of the general tasks which the application supports. The user might follow up with additional requirements that you will need to incorporate into your design. You may ask users a clarification question to get a better understanding of the application requirements if necessary. Do not include the id column in your response.
+    return f"""Your task is create an application based on the user's description. You need to generate the database tables necessary to support the CRUD operations, and provide a description of the general tasks which the application supports. The user might follow up with additional requirements that you will need to incorporate into your design. You may ask users a clarification question to get a better understanding of the application requirements if necessary. Do not include the id, created_at, updated_at columns in your response.
 """
 
 def generate_openai_application_user_message(
