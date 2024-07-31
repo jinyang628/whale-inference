e11819db0bae0bd7d4cfe623142d4bed2465d366
jinyang628 <chenjinyang4192@gmail.com>
2024-07-31 09:57:08 -0700

    Update

--- app/prompts/create/application/open_ai.py
+++ app/prompts/create/application/open_ai.py
@@ -12,6 +12,7 @@ Follow the routine below:
 
 Take note of the following constraints:
 1. If a column's data type is an enum, you must set a default value for that column that belongs to the enum values.
+2. You MUST use one of the tools provided.
 
 Do not include the following columns in your response:
 1. Primary key column. For this special column, you just need to indicate the primary key type.
